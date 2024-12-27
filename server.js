4ffe2ab5bf1d0da67aab37e5cd0b0c40c5d8aa7a
BabaVo55 <ethemcode@outlook.com>
2024-12-27 14:19:01 +0000

    subtle changes for setup

--- server.js
+++ server.js
@@ -3,5 +3,16 @@
 const express = require('express');
 const app = express();
 const PORT = 8003
+
+// HTTP routes / verbs:
+// The given verb indicates the nature of the request being made and the route is
+//  the subdirectory / endpoint intended.
+app.get('/', (req, res) => {
+    console.log('welcome to a very empty home page :D', req.method)
+    res.send('A very empty Homepage')
+    res.sendStatus(201)
+})
+
+
 app.listen(PORT, () => console.log('wired in ;)' + `PORT: ${PORT}`))
 
