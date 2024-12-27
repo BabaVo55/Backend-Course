6d5a309cb955a8e6442d09607ea923208c575d16
BabaVo55 <ethemcode@outlook.com>
2024-12-27 15:23:28 +0000

    html page request build

--- server.js
+++ server.js
@@ -4,20 +4,23 @@ const express = require('express');
 const app = express();
 const PORT = 8003
 
-// HTTP routes / verbs:
+// HTTP routes / verbs (method - endpoint):
 // The given verb indicates the nature of the request being made and the route is
 //  the subdirectory / endpoint intended.
 app.get('/', (req, res) => {
-    console.log('Network-Side: Welcome to a very empty home page :D', req.method)
-    
+    console.log('Network-Side: Welcome to a very empty home page :D', req.method)   
     // Cannot have multiple sends
     // res.send('A very empty Homepage')
     res.sendStatus(201)
 })
-
+//Dashboard endpoint / route
 app.get('/dashboard', (req, res) => {
     console.log('Dashboard page at your service')
     res.send('Hey there')
+})
+
+app.get('/home', (req, res) => {
+    res.send('<h1>Heres  youre new page cooking</h1>')
 })
 
 
