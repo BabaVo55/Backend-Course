5f3bafa8eca495e9f735e8124810c8ac3dd036e5
BabaVo55 <ethemcode@outlook.com>
2025-01-06 15:20:57 +0000

    back on the backend

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -4,14 +4,26 @@ import { fileURLToPath } from 'url';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+
+
+
+
+
+
+
+
+
+
+
 // Get the file path from the URL of the current module;
 const __filename = fileURLToPath(import.meta.url);
 
 // GEt the directory name from file path;
 const __dirname = dirname(__filename);
 
- // Middleware
- app.use(express.json())
+// Middleware
+app.use(express.json())
 
 // Serves the HTML file from the /public directory
 // Tells express to serve all files from the public folder as static assets / files
@@ -22,6 +34,17 @@ app.use(express.static(path.join(__dirname, "../public")))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"))
 })
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log('hello backend: Port: ' + PORT)
