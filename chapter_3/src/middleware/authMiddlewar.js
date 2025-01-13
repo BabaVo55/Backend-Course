435a8420ffebb4af0591248689c12deddcd73370
BabaVo55 <ethemcode@outlook.com>
2025-01-13 13:43:00 +0000

    stuck now

--- chapter_3/src/middleware/authMiddlewar.js
+++ chapter_3/src/middleware/authMiddlewar.js
@@ -1,1 +1,21 @@
- 
+ import jwt from 'jsonwebtoken';
+
+ function authMiddleware(req, res, next) {
+    const token = req.headers['Authorization']
+
+    if (!token){
+        return res.status(401).json({message: "no Token detected ;("})
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+        if (error){
+            return res.status(401).json({message: "invalid Token brother"});
+        } 
+
+        req.userId = decoded.id;
+        next();
+
+    })
+ }
+
+ export default authMiddleware;
